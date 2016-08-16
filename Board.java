829d8d77fc748c0f6258f543b95707e23f7b088b
joemoyer <joemoy11@gmail.com>
2016-08-16 13:33:01 -0400

    Add files via upload
    Will now display a window, the window can display images

--- Board.java
+++ Board.java
@@ -1,14 +1,86 @@
 package pack;
 
-import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Board extends JFrame{
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+@SuppressWarnings("serial")
+public class Board extends JPanel implements ActionListener{
+	
+	private Timer timer;
+	private final int DELAY = 10;
+	
+	static Image background;
 
 	public Board() {
-		initUI();
+		initBoard();
+		LoadImages();
 	}
 	
-	public void initUI() {
+	private void LoadImages() {
+		background = new ImageIcon("background.png").getImage();
+	}
+	
+	public void initBoard() {
+		addKeyListener(new TAdapter());
+		setFocusable(true);
+		setBackground(Color.GRAY);
+		
+		timer = new Timer(DELAY, this);
+		timer.start();
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		doDrawing(g);
+
+		Toolkit.getDefaultToolkit().sync();
+	}
+
+	private void doDrawing(Graphics g) {
+
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.drawImage(background, 250, 250, null);
+		g2d.drawLine(2, 2, 100, 100);
+	}
+	
+	private class TAdapter implements KeyListener {
+
+		@Override
+		public void keyPressed(KeyEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keyReleased(KeyEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keyTyped(KeyEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
 		
 	}
 }
